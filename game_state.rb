d4bb12bd910d63545ad5936b8a300582ce4a352c
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-04 11:27:18 -0800

    add game state class

--- game_state.rb
+++ game_state.rb
@@ -1,3 +1,35 @@
 class GameState
+  PLAYER1 = "Player 1"
+  PLAYER2 = "Player 2"
+
+  ## Access Controls
+  attr_reader :current_player
+  attr_accessor :game_over
+
   ## ContrucTOR
+  def initialize
+    @player1_lives = 3
+    @player2_lives = 3
+    @game_over = false
+    @current_player = PLAYER1
+  end
+
+  def get_score
+    "P1 #{@player1_lives}/3 P2 #{@player2_lives}/3"
+  end
+
+  def lose_life(current_player)
+    if current_player == PLAYER1
+      @player1_lives -= 1
+    elsif current_player == PLAYER2
+      @player2_lives -= 1
+    else
+      puts "Invalid player!"
+    end
+  end
+
+  def switch_player
+    @current_player = (current_player == PLAYER1) ? PLAYER2 : PLAYER1
+  end
+
 end
--- main.rb
+++ main.rb
@@ -1,12 +1,46 @@
 require 'pp'
 require './question'
+require './game_state'
+
+game = GameState.new
 
 ## Start game
-## 
+pp "Welcome to MATH SHOWDOWN!"
 
-q1 = Question.new
+## Loop logic
+loop do
+  current_player = game.current_player
 
-pp q1.question_string
+  ## create new question
+  question = Question.new
 
-pp q1.correct_answer?(gets.chomp)
+  ## Prompt current player and get answer
+  pp "#{current_player}: #{question.question_string}"
+  user_answer = gets.chomp
+
+  ## Answer verification
+  if question.correct_answer?(user_answer)
+    pp "NAILED IT, that's correct"
+  else
+    pp "That ain't right!"
+    game.lose_life(current_player)
+  end
+
+  ## Switch player & output current score
+  game.switch_player
+  pp game.get_score
+  pp "----- NEW TURN -----"
+
+end
+
+
+
+
+## Loop Logic
+
+# q1 = Question.new
+
+# pp q1.question_string
+
+# pp q1.correct_answer?(gets.chomp)
 
