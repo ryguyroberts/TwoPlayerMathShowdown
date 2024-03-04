528e2e27594ad9a647bb46b1d374fa8d8db069bd
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-04 11:43:03 -0800

    initial full game logic

--- game_state.rb
+++ game_state.rb
@@ -4,7 +4,9 @@ class GameState
 
   ## Access Controls
   attr_reader :current_player
-  attr_accessor :game_over
+  attr_reader :game_over
+  attr_reader :winner
+  
 
   ## ContrucTOR
   def initialize
@@ -12,17 +14,23 @@ class GameState
     @player2_lives = 3
     @game_over = false
     @current_player = PLAYER1
+    @winner = ""
   end
 
   def get_score
     "P1 #{@player1_lives}/3 P2 #{@player2_lives}/3"
   end
 
+  # Takes life away and sets winner if someone reaches zero and game over
   def lose_life(current_player)
     if current_player == PLAYER1
       @player1_lives -= 1
+      @game_over = true if @player1_lives == 0
+      @winner = PLAYER2
     elsif current_player == PLAYER2
       @player2_lives -= 1
+      @game_over = true if @player2_lives == 0
+      @winner = PLAYER1
     else
       puts "Invalid player!"
     end
@@ -31,5 +39,16 @@ class GameState
   def switch_player
     @current_player = (current_player == PLAYER1) ? PLAYER2 : PLAYER1
   end
+
+  # Winner string
+  def get_winner
+    if @winner == PLAYER1
+      "Player 1 wins with a score of #{@player1_lives}/3"
+    elsif @winner == PLAYER2
+      "Player 2 wins with a score of #{@player2_lives}/3"
+    else
+      puts "No winner"
+    end 
+  end 
 
 end
--- main.rb
+++ main.rb
@@ -1,11 +1,10 @@
-require 'pp'
 require './question'
 require './game_state'
 
 game = GameState.new
 
 ## Start game
-pp "Welcome to MATH SHOWDOWN!"
+puts "Welcome to MATH SHOWDOWN!"
 
 ## Loop logic
 loop do
@@ -15,32 +14,28 @@ loop do
   question = Question.new
 
   ## Prompt current player and get answer
-  pp "#{current_player}: #{question.question_string}"
+  puts "#{current_player}: #{question.question_string}"
   user_answer = gets.chomp
 
   ## Answer verification
   if question.correct_answer?(user_answer)
-    pp "NAILED IT, that's correct"
+    puts "NAILED IT, that's correct"
   else
-    pp "That ain't right!"
+    puts "That ain't right!"
     game.lose_life(current_player)
   end
 
+  ## check if game is over
+  if game.game_over
+    puts game.get_winner
+    puts "----- GAME OVER -----"
+    break
+  end
   ## Switch player & output current score
+  puts game.get_score
   game.switch_player
-  pp game.get_score
-  pp "----- NEW TURN -----"
+  puts "----- NEW TURN -----"
+  puts # just for a new line
 
 end
-
-
-
-
-## Loop Logic
-
-# q1 = Question.new
-
-# pp q1.question_string
-
-# pp q1.correct_answer?(gets.chomp)
 
